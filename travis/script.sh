00b47d5a165ee994c2456a1a38fef34fe134822f
nguyen-tinh-mulodo <nguyen.tinh@mulodo.com>
2013-07-16 11:22:08 +0700

    Update script.sh

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace Nhtinh_GuidBrowser.xcworkspace -scheme GuildBrowser build test
+xctool -workspace FrameWorks.xcworkspace -scheme Integation Tests build test
 #MyWorkspace MyScheme
