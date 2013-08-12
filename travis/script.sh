16c3a01bae63fbef0fc0604c60cc843fdb1ff758
nguyen-tinh-mulodo <nguyen.tinh@mulodo.com>
2013-08-12 10:52:48 +0700

    Update script.sh

--- travis/script.sh
+++ travis/script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-xctool -project LikeApp/LikeApp.xcodeproj -scheme TestTravis build test
-xctool -project LikeApp/LikeApp.xcodeproj -scheme TestGHUnit build test
-xctool -project LikeApp/LikeApp.xcodeproj -scheme LikeApp build test
+xctool -project LikeApp/LikeApp.xcodeproj -scheme TestTravis -sdk iphonesimulator  build
+xctool -project LikeApp/LikeApp.xcodeproj -scheme TestGHUnit -sdk iphonesimulator  build
+xctool -project LikeApp/LikeApp.xcodeproj -scheme LikeApp -sdk iphonesimulator  build test
 
 #xctool -workspace workspace.xcworkspace -scheme LikeApp build test
 #MyWorkspace MyScheme
